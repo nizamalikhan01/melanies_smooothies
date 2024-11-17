626840e74f4d4374a3acc5cf11da649fa26ff8dc
nizamalikhan01 <nizamalikhan01@gmail.com>
2024-11-17 18:10:20 +0530

    Update streamlit_app.py
    Added smoothfruit API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,7 @@ if ingredient_list:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered,' + name_on_order + '!!', icon="✅")
     st.write(my_insert_stmt)
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
